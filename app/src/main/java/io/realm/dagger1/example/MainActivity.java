cb4e42b3cb590a7fc57efea71e1f15ef72295fcd
TheFinestArtist <contact@thefinestartist.com>
2015-06-15 19:38:53 +0900

    Update

---
+++ app/src/main/java/io/realm/dagger1/example/MainActivity.java
@@ -0,0 +1,14 @@
+package io.realm.dagger1.example;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
